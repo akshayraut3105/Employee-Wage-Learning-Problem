af19d26f9f8e887084e97b5ccc3d718216d052d2
akshayraut3105 <akshayraut3105@gmail.com>
2021-04-23 14:09:55 +0530

    [AKSHAY] added method to check attendance

---
+++ UC1-CheckEmployeePresence.js
@@ -0,0 +1,14 @@
+console.log("Welcome to employee wage problem");
+//UC1
+const IS_ABSENT=0;
+{
+let empCheck=Math.floor(Math.random()*10)%2;
+if(empCheck==IS_ABSENT)
+{
+    console.log("Employee is Absent");
+}
+else
+{
+    console.log("Employee is Present");
+}
+}
